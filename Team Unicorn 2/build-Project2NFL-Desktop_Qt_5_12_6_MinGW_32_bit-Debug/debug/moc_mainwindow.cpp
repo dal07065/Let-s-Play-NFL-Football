893e5f202e4ba2bffc940eab724317d6191dbaca
Purdy <pu12dy@yahoo.com>
2020-12-02 16:05:42 -0800

    Final Version

---
+++ Team Unicorn 2/build-Project2NFL-Desktop_Qt_5_12_6_MinGW_32_bit-Debug/debug/moc_mainwindow.cpp
@@ -0,0 +1,208 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.12.6)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../Project-2-LetsPlayNFLFootball/mainwindow.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.12.6. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_MainWindow_t {
+    QByteArrayData data[21];
+    char stringdata0[529];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_MainWindow_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
+    {
+QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
+QT_MOC_LITERAL(1, 11, 21), // "on_show_Teams_clicked"
+QT_MOC_LITERAL(2, 33, 0), // ""
+QT_MOC_LITERAL(3, 34, 25), // "on_show_NFC_Teams_clicked"
+QT_MOC_LITERAL(4, 60, 25), // "on_show_AFC_Teams_clicked"
+QT_MOC_LITERAL(5, 86, 24), // "on_show_NFCNorth_clicked"
+QT_MOC_LITERAL(6, 111, 24), // "on_show_Stadiums_clicked"
+QT_MOC_LITERAL(7, 136, 30), // "on_show_StadiumsByDate_clicked"
+QT_MOC_LITERAL(8, 167, 25), // "on_show_Selection_clicked"
+QT_MOC_LITERAL(9, 193, 24), // "on_show_openRoof_clicked"
+QT_MOC_LITERAL(10, 218, 31), // "on_show_SeatingCapacity_clicked"
+QT_MOC_LITERAL(11, 250, 26), // "on_show_Conference_clicked"
+QT_MOC_LITERAL(12, 277, 28), // "on_show_BermudaGrass_clicked"
+QT_MOC_LITERAL(13, 306, 27), // "on_show_Selection_S_clicked"
+QT_MOC_LITERAL(14, 334, 23), // "on_actionExit_triggered"
+QT_MOC_LITERAL(15, 358, 24), // "on_actionAdmin_triggered"
+QT_MOC_LITERAL(16, 383, 25), // "on_actionLogOut_triggered"
+QT_MOC_LITERAL(17, 409, 21), // "on_pushButton_clicked"
+QT_MOC_LITERAL(18, 431, 34), // "on_actionAdmin_Functions_trig..."
+QT_MOC_LITERAL(19, 466, 33), // "on_actionReset_Database_trigg..."
+QT_MOC_LITERAL(20, 500, 28) // "on_actionLoad_Team_triggered"
+
+    },
+    "MainWindow\0on_show_Teams_clicked\0\0"
+    "on_show_NFC_Teams_clicked\0"
+    "on_show_AFC_Teams_clicked\0"
+    "on_show_NFCNorth_clicked\0"
+    "on_show_Stadiums_clicked\0"
+    "on_show_StadiumsByDate_clicked\0"
+    "on_show_Selection_clicked\0"
+    "on_show_openRoof_clicked\0"
+    "on_show_SeatingCapacity_clicked\0"
+    "on_show_Conference_clicked\0"
+    "on_show_BermudaGrass_clicked\0"
+    "on_show_Selection_S_clicked\0"
+    "on_actionExit_triggered\0"
+    "on_actionAdmin_triggered\0"
+    "on_actionLogOut_triggered\0"
+    "on_pushButton_clicked\0"
+    "on_actionAdmin_Functions_triggered\0"
+    "on_actionReset_Database_triggered\0"
+    "on_actionLoad_Team_triggered"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_MainWindow[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      19,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,  109,    2, 0x08 /* Private */,
+       3,    0,  110,    2, 0x08 /* Private */,
+       4,    0,  111,    2, 0x08 /* Private */,
+       5,    0,  112,    2, 0x08 /* Private */,
+       6,    0,  113,    2, 0x08 /* Private */,
+       7,    0,  114,    2, 0x08 /* Private */,
+       8,    0,  115,    2, 0x08 /* Private */,
+       9,    0,  116,    2, 0x08 /* Private */,
+      10,    0,  117,    2, 0x08 /* Private */,
+      11,    0,  118,    2, 0x08 /* Private */,
+      12,    0,  119,    2, 0x08 /* Private */,
+      13,    0,  120,    2, 0x08 /* Private */,
+      14,    0,  121,    2, 0x08 /* Private */,
+      15,    0,  122,    2, 0x08 /* Private */,
+      16,    0,  123,    2, 0x08 /* Private */,
+      17,    0,  124,    2, 0x08 /* Private */,
+      18,    0,  125,    2, 0x08 /* Private */,
+      19,    0,  126,    2, 0x08 /* Private */,
+      20,    0,  127,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<MainWindow *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->on_show_Teams_clicked(); break;
+        case 1: _t->on_show_NFC_Teams_clicked(); break;
+        case 2: _t->on_show_AFC_Teams_clicked(); break;
+        case 3: _t->on_show_NFCNorth_clicked(); break;
+        case 4: _t->on_show_Stadiums_clicked(); break;
+        case 5: _t->on_show_StadiumsByDate_clicked(); break;
+        case 6: _t->on_show_Selection_clicked(); break;
+        case 7: _t->on_show_openRoof_clicked(); break;
+        case 8: _t->on_show_SeatingCapacity_clicked(); break;
+        case 9: _t->on_show_Conference_clicked(); break;
+        case 10: _t->on_show_BermudaGrass_clicked(); break;
+        case 11: _t->on_show_Selection_S_clicked(); break;
+        case 12: _t->on_actionExit_triggered(); break;
+        case 13: _t->on_actionAdmin_triggered(); break;
+        case 14: _t->on_actionLogOut_triggered(); break;
+        case 15: _t->on_pushButton_clicked(); break;
+        case 16: _t->on_actionAdmin_Functions_triggered(); break;
+        case 17: _t->on_actionReset_Database_triggered(); break;
+        case 18: _t->on_actionLoad_Team_triggered(); break;
+        default: ;
+        }
+    }
+    Q_UNUSED(_a);
+}
+
+QT_INIT_METAOBJECT const QMetaObject MainWindow::staticMetaObject = { {
+    &QMainWindow::staticMetaObject,
+    qt_meta_stringdata_MainWindow.data,
+    qt_meta_data_MainWindow,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_MainWindow.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 19)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 19;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 19)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 19;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
